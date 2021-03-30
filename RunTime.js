b2b944e8c94f0b7838c6894d88cc072ba411b23b
Shankhadeep1234 <shankhadeepraj@gmail.com>
2021-03-30 13:35:34 +0530

    Runtime complexity

---
+++ RunTime.js
@@ -0,0 +1,59 @@
+const now = require("performance-now");
+
+//O(n) linear time
+//#region
+// const nemo = ["nemo"];
+// const everyone = [
+//   "dory",
+//   "bruce",
+//   "marlin",
+//   "nemo",
+//   "gill",
+//   "bloat",
+//   "nigel",
+//   "squirt",
+//   "darla",
+//   "hank",
+// ];
+
+// const large = new Array(100).fill("nemo");
+
+// function findNemo(array) {
+//   let t0 = now();
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === "nemo") {
+//       console.log("Found nemo!!!!");
+//       break;
+//     }
+//   }
+//   let t1 = now();
+//   console.log(`The function took ${t1 - t0} milliSeconds`);
+// }
+
+// findNemo(everyone); //o(n) --> linear time
+//#endregion
+
+//O(1) constant time
+//#region
+// const boxes = [0, 1, 2, 3, 4, 5, 6];
+// function countNumOfBoxes(boxes) {
+//   console.log(boxes[0]); //O(1)
+//   console.log(boxes[1]); //O(1)
+// }
+
+// countNumOfBoxes(boxes);
+//#endregion
+
+//O(n^2) Quadratic time
+//#region
+const nums = ["a", "b", "c", "d", "e"];
+
+function logAllPairsInArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j]);
+    }
+  }
+}
+
+logAllPairsInArray(nums);
