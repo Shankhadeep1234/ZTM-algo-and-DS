9c63a7cf7f7fdce6d4182264c423c1851e477d45
Shankhadeep1234 <shankhadeepraj@gmail.com>
2021-04-04 17:12:32 +0530

    added sample file for simple coding problems

---
+++ SampleCodingProb.js
@@ -0,0 +1,28 @@
+// Given 2 arrays, create a function that let's a user know (true/false) whether these two arrays contain any common items
+//For Example:
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'i'];
+//should return false.
+//-----------
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'x'];
+//should return true.
+
+// 2 parameters - arrays - no size limit
+// return true or false
+
+//#region  handShank approach O(n^2) "Quadratic"
+function compareTwo(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+//#endregion
+
+let array1 = ["a", "b", "c", "x"];
+let array2 = ["z", "y", "x"];
+
+compareTwo(array1, array2);
