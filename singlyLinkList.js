84a7d407496a6c9d36257972b4f14354eabc2305
Shankhadeep1234 <shankhadeepraj@gmail.com>
2021-04-25 13:46:59 +0530

    added insert to linkedList

--- singlyLinkList.js
+++ singlyLinkList.js
@@ -1,5 +1,5 @@
 //implementing linked list
-//10 -> 5 -> 16
+//1->10 ->5 -> 16
 
 // let myLinkedList = {
 //   head: {
@@ -13,6 +13,12 @@
 //     },
 //   },
 // };
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class LinkedList {
   constructor(value) {
@@ -24,10 +30,7 @@ class LinkedList {
     this.length = 1;
   }
   append(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
     console.log(newNode);
     this.tail.next = newNode;
     this.tail = newNode;
@@ -35,14 +38,44 @@ class LinkedList {
     return this;
   }
   prepend(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
     return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    //Check for proper parameters;
+    if (index >= this.length && index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
   }
 }
 
@@ -50,5 +83,8 @@ let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+myLinkedList.printList();
 
 // console.log(myLinkedList);
