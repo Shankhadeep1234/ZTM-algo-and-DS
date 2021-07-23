c5c4c27c0a671032351ea9b2792bf941cf26a97d
Shankhadeep1234 <shankhadeepraj@gmail.com>
2021-07-23 21:38:15 +0530

    added new file

--- sample.js
+++ sample.js
@@ -1,23 +1,20 @@
-/**
- * Flatten nested array using generator function 💡
- */
+const techBrands = [
+  {
+    name: "Google",
+    sharePrice: 1257,
+  },
+  {
+    name: "Facebook",
+    sharePrice: 768,
+  },
+  {
+    name: "Amazon",
+    sharePrice: 456,
+  },
+  {
+    name: "Netflix",
+    sharePrice: 657,
+  },
+];
 
-const nestedArray = [1, 2, [3, 4, [5, 6]], 7, 8];
-
-function* flatten(array, depth = 1) {
-  for (let item of array) {
-    if (Array.isArray(item) && depth) {
-      yield* flatten(item, depth - 1);
-    } else {
-      yield item;
-    }
-  }
-}
-
-const generator = flatten(nestedArray, Infinity);
-
-generator.next(); //{ value: 1, done: false }
-generator.next(); //{ value: 2, done: false }
-generator.next(); //{ value: 3, done: false }
-generator.next(); //{ value: 4, done: false }
-generator.next(); //{ value: 5, done: false }
+console.table(techBrands);
